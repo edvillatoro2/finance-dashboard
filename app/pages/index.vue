5cb9610144ea328b9b1ba1bc2a1f19f4bdd59a80
edvillatoro2 <edvillatoro2@gmail.com>
2026-03-16 14:19:19 -0400

    finiance app dashboard init

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Finance Dashboard</h1>
+    <h1>Welcome today is, {{ today }}</h1>
+    <section>stats go here</section>
+    <section>transactions go here</section>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const today = ref(new Date().toDateString());
+</script>
+
+<style scoped></style>
