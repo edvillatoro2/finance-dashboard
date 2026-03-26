f59a449b94f61a562f1767d78087cfeb8e26bf64
edvillatoro2 <edvillatoro2@gmail.com>
2026-03-26 11:22:17 -0400

    adds statCard component

---
+++ app/components/StatCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <h3>{{ title }}</h3>
+    <p>{{ value }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  value: number;
+}>();
+</script>
+
+<style scoped></style>
