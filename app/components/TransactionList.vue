ffa62b6b1137de9c3901dab4111e5ee3e9652832
edvillatoro2 <edvillatoro2@gmail.com>
2026-03-27 14:42:28 -0400

    transactionList logic

--- app/components/TransactionList.vue
+++ app/components/TransactionList.vue
@@ -1,7 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <TransactionItem
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :transaction="transaction"
+        @delete="emit('delete', $event)"
+      />
+    </ul>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TransactionItem from "./TransactionItem.vue";
+
+defineProps<{
+  transactions: any[];
+}>();
+
+const emit = defineEmits(["delete"]);
+</script>
 
 <style scoped></style>
