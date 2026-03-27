6aeb9ba1868ddf2fcbc832471c32250d1b0df98e
edvillatoro2 <edvillatoro2@gmail.com>
2026-03-27 14:38:02 -0400

    logic for transactionItem

--- app/components/TransactionItem.vue
+++ app/components/TransactionItem.vue
@@ -1,7 +1,25 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        <span>{{ transaction.text }}</span>
+        <span>{{ transaction.amount }}</span>
+        <button @click="emit('delete', transaction.id)">Delete</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{
+  transaction: {
+    id: number;
+    text: string;
+    amount: number;
+  };
+}>();
+
+const emit = defineEmits(["delete"]);
+</script>
 
 <style scoped></style>
