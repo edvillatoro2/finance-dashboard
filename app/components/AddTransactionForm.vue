34283239b6c3ce6def8ec37548b28bf536a23ec5
edvillatoro2 <edvillatoro2@gmail.com>
2026-03-27 14:00:58 -0400

    adds form

---
+++ app/components/AddTransactionForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <form action="">
+    <input v-model="text" placeholder="description" />
+    <input v-model.number="amount" type="number" />
+    <button>Add</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+const text = ref("");
+const amount = ref(0);
+</script>
+
+<style scoped></style>
