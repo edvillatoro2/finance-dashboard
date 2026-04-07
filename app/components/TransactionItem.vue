20b4182f08667100ac56e0953cd1d12a87eb20e9
edvillatoro2 <edvillatoro2@gmail.com>
2026-04-07 13:02:34 -0400

    updates transactionItem component logic

--- app/components/TransactionItem.vue
+++ app/components/TransactionItem.vue
@@ -1,32 +1,30 @@
 <template>
   <div>
-    <ul>
-      <li
-        class="flex justify-between items-center p-3 border rounded hover:bg-gray-50"
-      >
-        <div class="flex flex-col">
-          <span class="font-medium text-white">{{ transaction.text }}</span>
-          <span
-            :class="transaction.amount < 0 ? 'text-red-400' : 'text-green-400'"
-          >
-            {{ $currency(transaction.amount) }}
-          </span>
-          <button
-            @click="emit('delete', transaction.id)"
-            class="text-sm text-red-500 hover:text-red-700"
-          >
-            Delete
-          </button>
-        </div>
-      </li>
-    </ul>
+    <li
+      class="flex justify-between items-center p-3 border rounded hover:bg-gray-50"
+    >
+      <div class="flex flex-col">
+        <span class="font-medium text-white">{{ transaction.text }}</span>
+        <span
+          :class="transaction.amount < 0 ? 'text-red-400' : 'text-green-400'"
+        >
+          {{ $currency(transaction.amount) }}
+        </span>
+        <button
+          @click="emit('delete', transaction.id)"
+          class="text-sm text-red-500 hover:text-red-700"
+        >
+          Delete
+        </button>
+      </div>
+    </li>
   </div>
 </template>
 <script setup lang="ts">
 import type { Transaction } from "@/types/transaction";
 
 defineProps<{
-  transaction: Transaction;
+  transactions: Transaction;
 }>();
 
 const emit = defineEmits(["delete"]);
