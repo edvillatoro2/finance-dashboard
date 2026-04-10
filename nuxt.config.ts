13020f295a8eaf270cad1696a41fdabaa8369b60
edvillatoro2 <edvillatoro2@gmail.com>
2026-04-10 13:53:32 -0400

    prisma install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-charts"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-charts", "nuxt-auth-utils"],
 });
